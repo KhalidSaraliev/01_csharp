85400d6efcd2fd4001baaece5e2834d7aa953727
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-21 16:48:02 +0300

    add lesson:task1

--- Lesson3/Task1/Program.cs
+++ Lesson3/Task1/Program.cs
@@ -1,26 +1,11 @@
-﻿int i1 = 5;
-int i2 = 6;
-int sum = i1 + i2;
-Console.WriteLine(sum);
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
 
-long l1 = 6;
-long sum2 = l1 + i1;
-Console.WriteLine(sum2);
-
-double d1 = 5.5;
-double sum3 = d1 + i1;
-
-int product1 = i1 * i2;
-long product2 = l1 * i1;
-double product3 = d1 * i1;
-Console.WriteLine(product1);
-Console.WriteLine(product2);
-Console.WriteLine(product3);
-
-int quotient = i1 / i2;
-Console.WriteLine(quotient);
-
-double d2 = 6;
-double quotient2 = i1 / d2;
-Console.WriteLine(quotient2);
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
 
