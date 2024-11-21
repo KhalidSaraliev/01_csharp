55a1c3addb9925d9fb81f850bf0a8537bfd7268a
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-21 03:50:57 +0300

    add commit:task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] arr = { 3, 5, 35, 1, 7, 9, 43, 16, 77, 89 };
+int i = 0;
+
+while( i < arr.Length)
+{
+    if(arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
