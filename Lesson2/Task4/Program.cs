964dfa09bff63875583556d2ca7e8e3d442169ba
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-21 14:46:49 +0300

    add Lesson2:Task4

---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿int n = 5;
+int[] arr = { 6, 9, 4, 1, 78 };
+int i = 0;
+int max = arr[0];
+while(i < n)
+{
+    if(arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+max = arr[0];
+for(int j = 0; j < n; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+Console.WriteLine(max);
+
+max = arr[0];
+foreach(int e in arr)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
