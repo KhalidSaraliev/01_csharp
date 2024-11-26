dd8179ebeecd1ac01671d48ed856ad56055ae2ac
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-27 02:10:02 +0300

    add Lesson:Task1

---
+++ Lesson5/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int Fact(int n)
+{
+    if (n == 1 || n == 0) 
+    {
+        Console.WriteLine($"stop reqursion:n = {n}");
+        return 1;
+    }
+    Console.WriteLine (n);
+    return n * Fact(n - 1);
+}
+
+Console.WriteLine(Fact(5));
