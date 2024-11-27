d3de9681f1dc06c3b190064f2dc9323813285324
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-27 20:23:26 +0300

    work in repzitories lesso5

--- Lesson5/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Lesson5/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b00635598811c5b22ac4ae917e2bdb2e3ed21f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45547d48243da3ebc7bb0643e8283d90e3c12c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
