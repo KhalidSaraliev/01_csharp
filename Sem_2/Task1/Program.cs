f61f97779164aed44efea0210949ebcecabbf836
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-29 22:18:06 +0300

    added sem_2:task2

---
+++ Sem_2/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое трехзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int firstdigit = num / 100;
+    int thirdDigit = num % 10;
+    int result = firstdigit * 10 + thirdDigit;
+    Console.WriteLine ($"Первая и вторая цыфра введенного числа {result} ");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
---
+++ Sem_2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите трехзначного положительное целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    int secondtDigit = num / 10 % 10;
+    int lastDigit = num % 10;
+
+    int result = secondtDigit;
+    int count = 1;
+
+    while (count < lastDigit)
+    {
+        result = result * secondtDigit;
+        count++;
+    }
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Некорректиный ввод!");
+}
