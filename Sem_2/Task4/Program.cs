c54452a6f3b73d6577d831812ad92cf61c6f4222
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-30 00:08:15 +0300

    added sem_2:task4

---
+++ Sem_2/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите  целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+ 
+if ( num >= 100 || num <= -100)
+{
+    while (num >999 || num < -999)
+    {
+        num = num / 10;
+    }
+    int result = num % 10;
+    Console.WriteLine($"Третье число {result}");
+}
+else
+{
+    Console.WriteLine("Третьей цыфры нет");
+}
