aa0be0402b39b8098657e71d5d1e53bc23834ef7
Saraliev Khalid <saraliev_halid@mail.ru>
2024-11-30 22:48:28 +0300

    added Seminar3:Task2

--- Seminar3/Task2/Program.cs
+++ Seminar3/Task2/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int[] array = new int[10] {-10,5,8,4,-9,-3,-8,3,2,10,};
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= -1;
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
