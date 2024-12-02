8227abf13f7aa4db084786f740bb845604433004
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-02 13:12:28 +0300

    added all catalogs in sln

--- Lesson5/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Lesson5/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d06ca55ce73b7725394cf533936dfcb1406edc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0be0402b39b8098657e71d5d1e53bc23834ef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
