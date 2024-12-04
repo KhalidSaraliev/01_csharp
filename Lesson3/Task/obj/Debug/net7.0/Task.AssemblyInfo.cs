0dac56fe46a1160e9ed2bc3a8a9086155f1d6b64
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-05 02:22:53 +0300

    added new fails

--- Lesson3/Task/obj/Debug/net7.0/Task.AssemblyInfo.cs
+++ Lesson3/Task/obj/Debug/net7.0/Task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0be0402b39b8098657e71d5d1e53bc23834ef7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8227abf13f7aa4db084786f740bb845604433004")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
