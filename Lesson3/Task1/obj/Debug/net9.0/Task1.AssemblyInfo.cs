4cd99129f3bd52a3799c40b3ba1a779cd0b8877a
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-08 22:42:14 +0300

    commit

--- Lesson3/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Lesson3/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8227abf13f7aa4db084786f740bb845604433004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dac56fe46a1160e9ed2bc3a8a9086155f1d6b64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
