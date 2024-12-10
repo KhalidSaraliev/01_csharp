b2d53a717db28aa556ae4f2e235ed0ef05ef0472
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-10 11:09:14 +0300

    added seminar6:task2

---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’]
+
+
+char[] CreateMas(string tex)
+{
+    char[] newMassiv = new char[tex.Length];
+
+    for (int i = 0; i < tex.Length; i++)
+    {
+        newMassiv[i] = tex[i];
+    }
+    return newMassiv;
+}
+
+void PrintArray (char[] tex)
+{
+    Console.Write('[');
+    for (int i = 0; i < tex.Length; i++)
+    {
+        if (i < tex.Length - 1)
+        {
+            Console.Write($"{tex[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{tex[i]}");
+        }
+    }
+    Console.WriteLine(']');
+}
+
+char[] arr = CreateMas("Hello");
+PrintArray(arr);
