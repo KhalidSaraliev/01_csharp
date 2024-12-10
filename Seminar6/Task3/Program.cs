02b70ed8dd208f6610b019e7cc49015f7f3fa7f6
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-10 12:15:33 +0300

    added seminar6:task3

---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿int VowelsLetters(string str)
+{
+    string vowels = "aeyiou";
+    int count = 0;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (str[i] == vowels[j])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+Console.WriteLine("Введите строку строчными буквами");
+string start = Console.ReadLine();
+
+int result = VowelsLetters(start);
+Console.WriteLine($"В слове {VowelsLetters(start)} гласных ");
