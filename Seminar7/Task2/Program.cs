308c3ea1ef85da82e79428b21f6089a84a69de74
Saraliev Khalid <saraliev_halid@mail.ru>
2024-12-11 12:17:02 +0300

    added Seminar7:task2

---
+++ Seminar7/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit( int num)
+{
+    if(num == 0) return 0;
+    {
+        return num % 10 + SumDigit(num / 10);
+    }
+}
+
+Console.WriteLine(SumDigit(number));
