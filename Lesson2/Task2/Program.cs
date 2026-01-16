4da650df92fc60c0a88e970dec31693c60e22f7e
Saraliev Khalid <saraliev_halid@mail.ru>
2026-01-16 17:14:10 +0300

    add function for

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+//int n = 10;
+int[] arr = { 1, 43, 5, 6, 2, 34, 54, 67, 8, 9 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
+
+max = arr[0];
+for (int j = 0; j < n; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+
+Console.WriteLine(max);
