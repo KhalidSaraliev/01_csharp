73257fd59c20ab23c5ff5751a793083300b174ef
Saraliev Khalid <saraliev_halid@mail.ru>
2026-02-09 11:25:22 +0300

    just test

--- Lesson3/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ Lesson3/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c46360e25077d66e79b99154eb6745a8abba19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae2721bacfef5f8037257622ae0a21c4c71b061d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
